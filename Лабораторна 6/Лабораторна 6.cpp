44a2fb45ebaadf986b2b7fc8fc19076c176515fa
Yura <Yura@DESKTOP-VKM9FK8>
2023-12-02 18:44:30 +0200

    Добавьте файлы проекта.

---
+++ Лабораторна 6/Лабораторна 6.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <vector>
+
+template <typename T>
+void bubbleSort(std::vector<T>& arr) {
+    int n = arr.size();
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            
+            if (arr[j] > arr[j + 1]) {
+               
+                std::swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+}
+
+int main() {
+    
+    std::vector<int> numbers = { 64, 34, 25, 12, 22, 11, 90 };
+
+    std::cout << "Unsorted vector: ";
+    for (const auto& num : numbers) {
+        std::cout << num << " ";
+    }
+
+    bubbleSort(numbers);
+
+    std::cout << "\nSorted vector: ";
+    for (const auto& num : numbers) {
+        std::cout << num << " ";
+    }
+
+    return 0;
+}
